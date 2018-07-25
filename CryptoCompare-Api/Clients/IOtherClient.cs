d92ebc4a401e7a5697162144e0349a693e1020ee
Levent Saatçi <leventsaatci@hotmail.com>
2018-07-25 12:37:58 +0200

    IOtherclient documentation added

--- CryptoCompare-Api/Clients/IOtherClient.cs
+++ CryptoCompare-Api/Clients/IOtherClient.cs
@@ -5,8 +5,24 @@ namespace CryptoCompare_Api.Clients
 {
     public interface IOtherClient
     {
+        /// <summary>
+        /// Get CryptoCompare website, Facebook, code repository, Twitter and Reddit data for coins.
+        /// If called with the id of a cryptopian you just get data from our website that
+        /// is available to the public
+        /// </summary>
+        /// <param name="id">The id of the coin/exchange you want social data for</param>
+        /// <returns></returns>
         Task<Socialstat> GetSocialStat(int id);
+        /// <summary>
+        /// Get general info for all the coins available on the website.
+        /// </summary>
+        /// <returns></returns>
         Task<Coinlist> GetCoinList();
+        /// <summary>
+        /// Used to get all the mining equipment available on the website. It returns an array
+        /// of mining equipment objects
+        /// </summary>
+        /// <returns></returns>
         Task<Miningequipment> GetMiningEquipment();
     }
 }
