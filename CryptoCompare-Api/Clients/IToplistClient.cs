da7411d8e9ee569e55fac6ae49e60eeea16fc302
Levent Saatçi <leventsaatci@hotmail.com>
2018-07-27 12:27:14 +0200

    Toplist overload method and documentation added

--- CryptoCompare-Api/Clients/IToplistClient.cs
+++ CryptoCompare-Api/Clients/IToplistClient.cs
@@ -5,10 +5,92 @@ namespace CryptoCompare_Api.Clients
 {
     public interface IToplistClient
     {
-        Task<TopExchangesVolumeDataByPair> GetTopExchangesVolumeDataByPair(string fsym, string tsym, int? limit = null);
-        Task<TopExchangesFullDataByPair> GetTopExchangesFullDataByPair(string fsym, string tsym, int? limit = null);
-        Task<ToplistByPairVolume> GetToplistByPairVolume(string tsym, int? limit = null);
-        Task<ToplistofTradingPair> GetToplistOfTradingPairs(string fsym, int? limit = null);
-        Task<ToplistByTotalVolume> GetToplistByTotalVolume(string tsym, int? limit = null, int? page = null);
+        /// <summary>
+        /// Get top exchanges by volume for a currency pair.
+        /// The number of exchanges you get is the minimum of the limit you set (default 5) and the total number of exchanges available
+        /// </summary>
+        /// <param name="fsym">REQUIRED The cryptocurrency symbol of interest [Max character length: 10]</param>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <returns></returns>
+        Task<TopExchangesVolumeDataByPair> GetTopExchangesVolumeDataByPair(string fsym, string tsym);
+
+        /// <summary>
+        /// Get top exchanges by volume for a currency pair.
+        /// The number of exchanges you get is the minimum of the limit you set (default 5) and the total number of exchanges available
+        /// </summary>
+        /// <param name="fsym">REQUIRED The cryptocurrency symbol of interest [Max character length: 10]</param>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <param name="limit">The number of data points to return</param>
+        /// <returns></returns>
+        Task<TopExchangesVolumeDataByPair> GetTopExchangesVolumeDataByPair(string fsym, string tsym, int? limit);
+
+        /// <summary>
+        /// Get top exchanges by volume for a currency pair plus the full CCCAGG data.
+        /// The number of exchanges you get is the minimum of the limit you set (default 5) and the total number of exchanges available
+        /// </summary>
+        /// <param name="fsym">REQUIRED The cryptocurrency symbol of interest [Max character length: 10]</param>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <returns></returns>
+        Task<TopExchangesFullDataByPair> GetTopExchangesFullDataByPair(string fsym, string tsym);
+
+        /// <summary>
+        /// Get top exchanges by volume for a currency pair plus the full CCCAGG data.
+        /// The number of exchanges you get is the minimum of the limit you set (default 5) and the total number of exchanges available
+        /// </summary>
+        /// <param name="fsym">REQUIRED The cryptocurrency symbol of interest [Max character length: 10]</param>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <param name="limit">The number of data points to return</param>
+        /// <returns></returns>
+        Task<TopExchangesFullDataByPair> GetTopExchangesFullDataByPair(string fsym, string tsym, int? limit);
+
+        /// <summary>
+        /// Get top coins by volume for the to currency. It returns volume24hto and total supply (where available).
+        /// The number of coins you get is the minimum of the limit you set (default 50) and the total number of coins available
+        /// </summary>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <returns></returns>
+        Task<ToplistByPairVolume> GetToplistByPairVolume(string tsym);
+
+        /// <summary>
+        /// Get top coins by volume for the to currency. It returns volume24hto and total supply (where available).
+        /// The number of coins you get is the minimum of the limit you set (default 50) and the total number of coins available
+        /// </summary>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <param name="limit">The number of data points to return</param>
+        /// <returns></returns>
+        Task<ToplistByPairVolume> GetToplistByPairVolume(string tsym, int? limit);
+
+        /// <summary>
+        /// Get top pairs by volume for a currency (always uses our aggregated data).
+        /// The number of pairs you get is the minimum of the limit you set (default 5) and the total number of pairs available
+        /// </summary>
+        /// <param name="fsym">REQUIRED The cryptocurrency symbol of interest [Max character length: 10]</param>
+        /// <returns></returns>
+        Task<ToplistofTradingPair> GetToplistOfTradingPairs(string fsym);
+
+        /// <summary>
+        /// Get top pairs by volume for a currency (always uses our aggregated data).
+        /// The number of pairs you get is the minimum of the limit you set (default 5) and the total number of pairs available
+        /// </summary>
+        /// <param name="fsym">REQUIRED The cryptocurrency symbol of interest [Max character length: 10]</param>
+        /// <param name="limit">The number of data points to return</param>
+        /// <returns></returns>
+        Task<ToplistofTradingPair> GetToplistOfTradingPairs(string fsym, int? limit);
+
+        /// <summary>
+        /// Get a number of top coins by their total volume accross all markets in the last 24 hours. Deafult value is first page (0) and the top 10 coins.
+        /// </summary>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <returns></returns>
+        Task<ToplistByTotalVolume> GetToplistByTotalVolume(string tsym);
+
+        /// <summary>
+        /// Get a number of top coins by their total volume accross all markets in the last 24 hours. Deafult value is first page (0) and the top 10 coins.
+        /// </summary>
+        /// <param name="tsym">REQUIRED The currency symbol to convert into [Max character length: 10]</param>
+        /// <param name="limit">The number of data points to return</param>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        Task<ToplistByTotalVolume> GetToplistByTotalVolume(string tsym, int? limit, int? page);
     }
 }
--- CryptoCompare-Api/Clients/ToplistClient.cs
+++ CryptoCompare-Api/Clients/ToplistClient.cs
@@ -11,34 +11,63 @@ namespace CryptoCompare_Api.Clients
         {
         }
 
+        public async Task<TopExchangesVolumeDataByPair> GetTopExchangesVolumeDataByPair(string fsym, string tsym)
+        {
+            return await GetAsync<TopExchangesVolumeDataByPair>(
+                ToplistApiUrls.TopExchangesVolumeDataByPair(fsym, tsym, null)).ConfigureAwait(false);
+        }
+
         public async Task<TopExchangesVolumeDataByPair> GetTopExchangesVolumeDataByPair(string fsym, string tsym,
-            int? limit = null)
+            int? limit)
         {
             return await GetAsync<TopExchangesVolumeDataByPair>(
                 ToplistApiUrls.TopExchangesVolumeDataByPair(fsym, tsym, limit)).ConfigureAwait(false);
         }
 
+        public async Task<TopExchangesFullDataByPair> GetTopExchangesFullDataByPair(string fsym, string tsym)
+        {
+            return await GetAsync<TopExchangesFullDataByPair>(
+                ToplistApiUrls.TopExchangesFullDataByPair(fsym, tsym, null)).ConfigureAwait(false);
+        }
+
         public async Task<TopExchangesFullDataByPair> GetTopExchangesFullDataByPair(string fsym, string tsym,
-            int? limit = null)
+            int? limit)
         {
             return await GetAsync<TopExchangesFullDataByPair>(
                 ToplistApiUrls.TopExchangesFullDataByPair(fsym, tsym, limit)).ConfigureAwait(false);
         }
 
-        public async Task<ToplistByPairVolume> GetToplistByPairVolume(string tsym, int? limit = null)
+        public async Task<ToplistByPairVolume> GetToplistByPairVolume(string tsym)
+        {
+            return await GetAsync<ToplistByPairVolume>(ToplistApiUrls.ToplistByPairVolume(tsym, null))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<ToplistByPairVolume> GetToplistByPairVolume(string tsym, int? limit)
         {
             return await GetAsync<ToplistByPairVolume>(ToplistApiUrls.ToplistByPairVolume(tsym, limit))
                 .ConfigureAwait(false);
         }
 
-        public async Task<ToplistofTradingPair> GetToplistOfTradingPairs(string fsym, int? limit = null)
+        public async Task<ToplistofTradingPair> GetToplistOfTradingPairs(string fsym)
+        {
+            return await GetAsync<ToplistofTradingPair>(ToplistApiUrls.ToplistOfTradingPairs(fsym, null))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<ToplistofTradingPair> GetToplistOfTradingPairs(string fsym, int? limit)
         {
             return await GetAsync<ToplistofTradingPair>(ToplistApiUrls.ToplistOfTradingPairs(fsym, limit))
                 .ConfigureAwait(false);
         }
 
-        public async Task<ToplistByTotalVolume> GetToplistByTotalVolume(string tsym, int? limit = null,
-            int? page = null)
+        public async Task<ToplistByTotalVolume> GetToplistByTotalVolume(string tsym)
+        {
+            return await GetAsync<ToplistByTotalVolume>(ToplistApiUrls.ToplistByTotalVolume(tsym, null, null));
+        }
+
+        public async Task<ToplistByTotalVolume> GetToplistByTotalVolume(string tsym, int? limit,
+            int? page)
         {
             return await GetAsync<ToplistByTotalVolume>(ToplistApiUrls.ToplistByTotalVolume(tsym, limit, page));
         }
